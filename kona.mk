9dc40279d211840edd12fa15623198abb7c00d29
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    sm8250-common: Add bootctrl to PRODUCT_SOONG_NAMESPACES
    Change-Id: Id6174b9a109b0ca5633535cd35de9157789a23cb
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kona.mk
+++ kona.mk
@@ -342,6 +342,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/bootctrl \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
